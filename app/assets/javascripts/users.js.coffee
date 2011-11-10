4749d9fb02e783ba377cb16848e704fab7feb269
feipinghuang <huang900107@163.con>
2011-11-10 12:29:08 +0800

    onfollow

--- app/assets/javascripts/users.js.coffee
+++ app/assets/javascripts/users.js.coffee
@@ -1,3 +1,24 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+jQuery ->
+  $('a#follow').click ->
+    link = $(this)
+    $.get this.href, (data) ->
+      if data.followed
+        link.removeClass('green').addClass('white').html("取消关注")
+      else
+        link.removeClass('white').addClass('green').html("关注")
+    false
+  
+  $('a#favorite-question').click ->
+    link = $(this)
+    $.get this.href, (data) ->
+      if data.favorited
+        link.removeClass('green').addClass('white').html("取消收藏")
+      else
+        link.removeClass('white').addClass('green').html "收藏"
+    false
+  
+  false
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,43 @@
 class UsersController < ApplicationController
   can_edit_on_the_spot
-  
   def show
     @user = User.find params[:id]
   end
+  
+  def follow_question
+    question = Question.find_by_id params[:qid]
+    if question && current_user.follow_question!(question)
+      render nothing: true
+    else
+      render :json => {status: :unprocessable_entity}
+    end
+  end
+  
+  def unfollow_question
+    question = Question.find_by_id params[:qid]
+    if question && current_user.unfollow_question!(question)
+      render nothing: true
+    else
+      render :json => {status: :unprocessable_entity}
+    end
+  end
+  
+  def follow_user
+    user = User.find_by_id params[:uid]
+    if user && current_user.follow_user!(user)
+      render nothing: true
+    else
+      render :json => {status: :unprocessable_entity}
+    end
+  end
+  
+  def unfollow_user
+    user = User.find_by_id params[:uid]
+    if user && current_user.unfollow_user!(user)
+      render nothing: true
+    else
+      render :json => {status: :unprocessable_entity}
+    end
+  end
+  
 end
